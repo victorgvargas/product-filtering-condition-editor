8de91c403c8847c958353e35e3467efffddb1144
Victor <vvargas2013@gmail.com>
2025-04-11 12:27:55 +0100

    implemented clear filters

--- src/components/ProductsTable.tsx
+++ src/components/ProductsTable.tsx
@@ -15,9 +15,11 @@ const ProductsTable: React.FC<ProductsTableProps> = ({
   properties,
 }) => {
   const [filteredProducts, setFilteredProducts] = useState(products);
-  const [selectedProperty, setSelectedProperty] = useState<Property>();
-  const [selectedOperator, setSelectedOperator] = useState<Operator>();
-  const [propertyValues, setPropertyValues] = useState<PropertyValue[]>();
+  const [selectedProperty, setSelectedProperty] = useState<Property | null>();
+  const [selectedOperator, setSelectedOperator] = useState<Operator | null>();
+  const [propertyValues, setPropertyValues] = useState<
+    PropertyValue[] | null
+  >();
 
   const findProperty = (id: number) => {
     return (
@@ -45,6 +47,12 @@ const ProductsTable: React.FC<ProductsTableProps> = ({
       .flat();
   };
 
+  const clearFilters = () => {
+    setSelectedOperator(null);
+    setSelectedProperty(null);
+    setPropertyValues(null);
+  };
+
   useEffect(() => {
     if (selectedProperty && selectedOperator) {
       setPropertyValues(findPropertyValues(selectedProperty.id));
@@ -54,60 +62,69 @@ const ProductsTable: React.FC<ProductsTableProps> = ({
   return (
     <section className={styles["products"]}>
       <div className={styles["products-filters"]}>
-        <div className={styles["products-filters-filter"]}>
-          <select
-            name="properties"
-            id="properties"
-            onChange={(event) =>
-              setSelectedProperty(findProperty(Number(event.target.value)))
-            }
-          >
-            <optgroup label="Properties">
-              {properties.map((property) => (
-                <option key={property.id} value={property.id}>
-                  {property.name}
-                </option>
-              ))}
-            </optgroup>
-          </select>
-        </div>
-
-        <div className={styles["products-filters-filter"]}>
-          <select
-            name="property-values"
-            id="property-values"
-            onChange={(event) =>
-              setSelectedOperator(findOperator(event.target.value))
-            }
-          >
-            <optgroup label="Property Values">
-              {filtersService.operators.map((operator) => (
-                <option key={operator.id} value={operator.id}>
-                  {operator.text}
-                </option>
-              ))}
-            </optgroup>
-          </select>
-        </div>
-        {propertyValues && (
+        <div className={styles["products-filters-container"]}>
           <div className={styles["products-filters-filter"]}>
             <select
-              name="operators"
-              id="operators"
+              name="properties"
+              id="properties"
               onChange={(event) =>
-                setSelectedOperator(findOperator(event.target.value))
+                setSelectedProperty(findProperty(Number(event.target.value)))
               }
             >
-              <optgroup label="Operators">
-                {propertyValues.map((propVal, index) => (
-                  <option key={index} value={propVal.property_id}>
-                    {propVal.value}
+              <optgroup label="Properties">
+                {properties.map((property) => (
+                  <option key={property.id} value={property.id}>
+                    {property.name}
                   </option>
                 ))}
               </optgroup>
             </select>
           </div>
-        )}
+
+          <div className={styles["products-filters-filter"]}>
+            <select
+              name="property-values"
+              id="property-values"
+              onChange={(event) =>
+                setSelectedOperator(findOperator(event.target.value))
+              }
+            >
+              <optgroup label="Property Values">
+                {filtersService.operators.map((operator) => (
+                  <option key={operator.id} value={operator.id}>
+                    {operator.text}
+                  </option>
+                ))}
+              </optgroup>
+            </select>
+          </div>
+          {propertyValues && (
+            <div className={styles["products-filters-filter"]}>
+              <select
+                name="operators"
+                id="operators"
+                onChange={(event) =>
+                  setSelectedOperator(findOperator(event.target.value))
+                }
+              >
+                <optgroup label="Operators">
+                  {propertyValues.map((propVal, index) => (
+                    <option key={index} value={propVal.property_id}>
+                      {propVal.value}
+                    </option>
+                  ))}
+                </optgroup>
+              </select>
+            </div>
+          )}
+        </div>
+
+        <button
+          className={styles["products-filters-clear"]}
+          onClick={clearFilters}
+        >
+          Clear filters
+        </button>
       </div>
 
       <table className={styles["products-table"]}>
